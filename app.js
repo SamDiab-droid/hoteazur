346d73fc45780ac0bd05435587fb5b263b11595b
SamDiab-droid <sam.diab.2000@gmail.com>
2025-12-21 20:24:35 -0500

    updating button functionality

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 function bookNow() {
-    alert("Thank you! Redirecting you to our booking page...");
     // Replace this with your actual booking page URL:
     window.location.href = "https://www.booking.com/hotel/fr/cozy-downtown-room-near-the-beach-with-private-bath.html?chal_t=1766291207941&force_referer=";
 }
